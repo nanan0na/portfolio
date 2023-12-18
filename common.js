8060dc2e1a7f5824dfa02bd56b3d376a945c793c
nana <goldbarrr5@gmail.com>
2023-12-18 18:15:31 +0900

    마우스 커서

---
+++ common.js
@@ -0,0 +1,38 @@
+$(function () {
+  // cursor
+  const $window = $(window);
+  const $cursor = $('.cursor');
+  const $point = $('a, button, label, .info');
+
+  $window.on('mousemove', function (e) {
+    let mouseX = e.pageX;
+    let mouseY = e.pageY;
+    /* console.log(e); */
+    $cursor.css({
+      left: mouseX,
+      top: mouseY,
+    });
+  });
+
+  $window.on('mousedown', function () {
+    $cursor.addClass('click');
+  });
+
+  $point.on('mouseover', function () {
+    $cursor.addClass('point');
+  });
+  $point.on('mouseout', function () {
+    $cursor.removeClass('point');
+  });
+
+  $window.on('mouseup', function () {
+    $cursor.removeClass('click');
+  });
+
+  $window.on('click', function () {
+    $cursor.addClass('click');
+    setTimeout(function () {
+      $cursor.removeClass('click');
+    }, 400);
+  });
+});
