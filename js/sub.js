61f773440fad4f5f9fcb1218c1ad0efbb6aa9df2
nana <goldbarrr5@gmail.com>
2023-12-21 17:11:02 +0900

    슬라이드 연동이 왜 언 돼

--- js/sub.js
+++ js/sub.js
@@ -2,9 +2,11 @@ $(function () {
   /* web 슬라이드 */
   const conList1 = new Swiper('.con-list1', {
     loop: true,
-    autoplay: {
-      delay: 300,
-    },
+    loopedSlides: 1,
+    // autoplay: {
+    //   delay: 300,
+    //   /*  pauseOnMouseEnter: true, */
+    // },
     slidesPerView: 4,
     spaceBetween: 0,
     navigation: {
@@ -14,10 +16,16 @@ $(function () {
     slideToClickedSlide: true,
   });
   const conList2 = new Swiper('.con-list2', {
+    loop: true,
+    loopedSlides: 1,
     slidesPerView: 1,
     grabCursor: true,
-    pauseOnMouseEnter: true,
   });
   conList1.controller.control = conList2;
   conList2.controller.control = conList1;
+
+  // $('.con-list1 li:eq(2)').ready($('.web h2').text('RESPONSIVE WEB'));
+  // $('.con-list1 li:eq(2)').on('ready', function () {
+  // $('.web h2').text('RESPONSIVE WEB');
+  // });
 });
